25a183142d4aa963fb0db10611464790be9beff1
Raj Powar <rpowar@Rajs-Air.lan>
2015-09-08 17:08:39 -0700

    server file

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
+var app = express();
+
+//scrapping logic
+app.get('/scrape', function(req, res){
+	var url = 'http://www.imdb.com/title/tt1229340/';
+
+	request(url, function(error, response, html){
+
+		if(!error){
+			var $ = cheerio.load(html);
+
+			var title, release, rating;
+			var json = {title:"", release:"", rating:""};
+
+			$('.header').filter(function(){
+				var data = $(this);
+
+				title = data.children().first().text();
+				release = data.children().last().children().text();
+				json.title = title;
+				json.release = release;
+
+			});
+
+			$('.star-box-giga-star').filter(function(){
+				var data = $(this);
+				rating = data.text();
+				json.rating = rating;
+			});
+
+		}
+
+		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(){
+			console.log('File successfully written');
+		});
+
+		res.send('Check your console!');
+	});
+});
+
+app.listen('8081')
+
+exports = module.exports = app;
